96d5d2db2b6fd0c7671505573598dbd5ae26df8d
Michael Tuszynski <miketuszynski42@gmail.com>
2023-08-23 21:39:22 -0700

    initial commit - working frontend with todo title and status

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+type Todo = {
+    _id: string;
+    title: string;
+    completed: boolean;
+};
+
+type TodoItemProps = {
+    todo: Todo;
+    onTodoUpdate: (id: string, updatedTodo: Todo) => void;
+    onDelete: (id: string) => void;
+};
+
+const TodoItem: React.FC<TodoItemProps> = ({ todo, onTodoUpdate, onDelete }) => {
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [editedTitle, setEditedTitle] = useState<string>(todo.title);
+
+    const toggleCompleted = async () => {
+        const updatedTodo = { ...todo, completed: !todo.completed };
+        await axios.put(`http://localhost:5000/todos/${todo._id}`, updatedTodo);
+        onTodoUpdate(todo._id, updatedTodo);
+    };
+
+    const startEditing = () => {
+        setIsEditing(true);
+    };
+
+    const handleEdit = async () => {
+        const updatedTodo = { ...todo, title: editedTitle };
+        await axios.put(`http://localhost:5000/todos/${todo._id}`, updatedTodo);
+        onTodoUpdate(todo._id, updatedTodo);
+        setIsEditing(false);
+    };
+
+    const handleDelete = async () => {
+        await axios.delete(`http://localhost:5000/todos/${todo._id}`);
+        onDelete(todo._id);
+    };
+
+    return (
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+            {isEditing ? (
+                <>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={editedTitle}
+                        onChange={e => setEditedTitle(e.target.value)}
+                    />
+                    <button className="btn btn-success ml-2" onClick={handleEdit}>Save</button>
+                </>
+            ) : (
+                <>
+                    <div>
+                        <input
+                            type="checkbox"
+                            className="mr-2"
+                            checked={todo.completed}
+                            onChange={toggleCompleted}
+                        />
+                        <span className={todo.completed ? 'text-muted text-decoration-line-through' : ''}>&nbsp;&nbsp;
+                            {todo.title}
+                        </span>
+                    </div>
+                    <div>
+                        <button className="btn btn-warning btn-sm mr-2" onClick={startEditing}>Edit</button>
+                        <button className="btn btn-danger btn-sm" onClick={handleDelete}>Delete</button>
+                    </div>
+                </>
+            )}
+        </li>
+    );
+}
+
+export default TodoItem;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import TodoItem from './TodoItem';
+
+type Todo = {
+    _id: string;
+    title: string;
+    completed: boolean;
+};
+
+const TodoList: React.FC = () => {
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [title, setTitle] = useState<string>('');
+
+    useEffect(() => {
+        async function fetchTodos() {
+            const response = await axios.get<Todo[]>('http://localhost:5000/todos');
+            setTodos(response.data);
+        }
+        fetchTodos();
+    }, []);
+
+    const addTodo = async () => {
+        const response = await axios.post<Todo>('http://localhost:5000/todos', { title });
+        setTodos([...todos, response.data]);
+        setTitle('');
+    };
+
+    const handleTodoUpdate = (id: string, updatedTodo: Todo) => {
+        const updatedTodos = todos.map(todo => (todo._id === id ? updatedTodo : todo));
+        setTodos(updatedTodos);
+    };
+
+    const handleDelete = (id: string) => {
+        const updatedTodos = todos.filter(todo => todo._id !== id);
+        setTodos(updatedTodos);
+    };
+
+    return (
+        <div className="container mt-5">
+            <h1 className="text-center mb-4">ToDo App</h1>
+            <div className="input-group mb-3">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Add a new task"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                />
+                <div className="input-group-append">
+                    <button className="btn btn-primary" onClick={addTodo}>Add</button>
+                </div>
+            </div>
+            <ul className="list-group">
+                {todos.map(todo => (
+                    <TodoItem
+                        key={todo._id}
+                        todo={todo}
+                        onTodoUpdate={handleTodoUpdate}
+                        onDelete={handleDelete}
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default TodoList;
