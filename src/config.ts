629b8b1e98effe8d9bdc4f571a13cfeaf46e1612
Michael Tuszynski <miketuszynski42@gmail.com>
2023-10-19 09:08:06 -0700

    fix fetch 3

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,18 @@
 let configExports: { REACT_APP_BACKEND_URL?: string } = {};
 
 export const loadConfig = async () => {
-    const response = await fetch('./config.json');
-    const configJson = await response.json();
-    configExports.REACT_APP_BACKEND_URL = `https://${configJson.REACT_APP_BACKEND_URL}:5000`;
+    try {
+        const response = await fetch('/config.json');
+        if (!response.ok) {
+            console.error('Failed to load config.json with status:', response.status);
+            return;
+        }
+        const configJson = await response.json();
+        configExports.REACT_APP_BACKEND_URL = `https://${configJson.REACT_APP_BACKEND_URL}:5000`;
+    } catch (error) {
+        console.error('Error loading config.json:', error);
+    }
 }
+
 
 export default configExports;
