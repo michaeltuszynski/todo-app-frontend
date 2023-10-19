f5cc553b0db33779e012680d5277e5a6252d7e1e
Michael Tuszynski <miketuszynski42@gmail.com>
2023-10-19 08:52:25 -0700

    fix fetch

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 let configExports: { REACT_APP_BACKEND_URL?: string } = {};
 
 export const loadConfig = async () => {
-    const response = await fetch('config.json');
+    const response = await fetch('./config.json');
     const configJson = await response.json();
     configExports.REACT_APP_BACKEND_URL = `https://${configJson.REACT_APP_BACKEND_URL}:5000`;
 }
